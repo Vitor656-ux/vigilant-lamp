ebe321e0348174dced27a5abb36256456066be34
Vitor656-ux <00001215173775sp@al.educacao.sp.gov.br>
2025-06-16 14:31:38 -0300

    Update exercicios07.py

--- exercicios07.py
+++ exercicios07.py
@@ -1,16 +1,59 @@
-import json
-import os
+Nome completo: w
+Idade: 30
+Espécie: robô de desmonte
+Gênero: masculino
+Altura: 2 m
+Cor dos olhos: Amarelo
+Ocupação: legista
 
-Data = ('data.json')
+Host principal do Solucionador Absoluto
+Afiliação
+Solucionador Absoluto
 
-def carregar_dados():
-    if os.path.exists(Data):
-        with open(Data, 'r', encoding= 'utf-8') as arq_json: 
-            return json.load(arq_json)
-    else:
-        return []
-    
-clientes = carregar_dados()
+Parentes
+N (irmão)
+Cyn (irmã)
+Amigos
+V
+Estado
+Ativo
+Outras informações
+Gosta
+	Café
+	Doces
+	Ler livros
+Desagrados
+	Capuccino
+	Humanos 
+	Mãe da Tessa
+	Uzi
+	J
+	Z
+Poderes/Habilidades
 
-for cliente in clientes:
-    print(clientes ['nome_completo'])
+
+
+
+
+
+Superforça
+Voo
+
+
+
+Proficiência em estrela ninja
+Precisão extrema
+
+Garras retráteis
+Lâminas retráteis
+Motosserras retráteis
+Canhão de mísseis
+Metralhadora retrátil
+Cortador a laser
+Cauda ácida
+Auto-regeneração
+Saliva curativa
+
+
+
+
